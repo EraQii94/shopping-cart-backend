a27ab8a55a97418e4ff306431e9e7b2ce7c0eca6
EraQii94 <kabdulrahman115@gmail.com>
2025-03-16 16:49:39 +0200

    .

---
+++ src/main/java/com/eCommerceDemo/dream_shops/model/Category.java
@@ -0,0 +1,28 @@
+package com.eCommerceDemo.dream_shops.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+}
